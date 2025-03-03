49de4070e31aca0771ce9df571ec746067554d36
darvie <ddhill01@student.ubc.ca>
2025-03-02 23:46:17 -0800

    Dash implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,11 +8,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float gravityMultiplier = 2.5f;
     [SerializeField] private float lowJumpMultiplier = 2f;
     [SerializeField] private Transform cameraTransform;
-    [SerializeField] private float groundCheckDistance = 0.2f;
+
+    [SerializeField] private float dashSpeed = 15f;  
+    [SerializeField] private float dashDuration = 0.2f; 
+    [SerializeField] private float dashCooldown = 1f; 
 
     private Rigidbody playerRB;
     private bool isGrounded = true;
     private int jumpCount = 0;
+    private bool isDashing = false;
+    private float dashTime = 0f;
+    private float lastDashTime = -Mathf.Infinity;
 
     void Start()
     {
@@ -31,6 +37,22 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
         ApplyBetterGravity();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Time.time >= lastDashTime + dashCooldown && !isDashing)
+        {
+            StartDash();
+        }
+
+        // Handle Dash Timer
+        if (isDashing)
+        {
+            dashTime -= Time.deltaTime;
+            if (dashTime <= 0)
+            {
+                StopDash();
+            }
+        }
+
     }
     private void MovePlayer()
     {
@@ -96,14 +118,37 @@ public class PlayerController : MonoBehaviour
     {
         if (!isGrounded)
         {
-            if (playerRB.linearVelocity.y < 0) // Falling down
+            if (playerRB.linearVelocity.y < 0) 
             {
                 playerRB.linearVelocity += Vector3.up * Physics.gravity.y * (gravityMultiplier - 1) * Time.deltaTime;
             }
-            else if (playerRB.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space)) // If player lets go of jump early
+            else if (playerRB.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
             {
                 playerRB.linearVelocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
             }
         }
+    }
+
+    private void StartDash()
+    {
+        if (Time.time >= lastDashTime + dashCooldown && !isDashing)
+        {
+            isDashing = true;
+            dashTime = dashDuration;  
+            lastDashTime = Time.time; 
+
+          
+            Vector3 dashDirection = cameraTransform.forward * Input.GetAxis("Vertical") + cameraTransform.right * Input.GetAxis("Horizontal");
+            dashDirection.y = 0;  
+            dashDirection.Normalize();
+
+            playerRB.linearVelocity = new Vector3(dashDirection.x * dashSpeed, playerRB.linearVelocity.y, dashDirection.z * dashSpeed);
+        }
+    }
+
+    private void StopDash()
+    {
+        isDashing = false;
+        playerRB.linearVelocity = new Vector3(playerRB.linearVelocity.x, playerRB.linearVelocity.y, playerRB.linearVelocity.z);
     }
 }
